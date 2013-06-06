ce95830b36fc32c23d1b9574c8f4cc2ee2402db8
Dan Moutal <dan@moutal.ca>
2013-06-05 20:14:51 -0700

    Improve meta display

--- content-blogs.php
+++ content-blogs.php
@@ -9,7 +9,7 @@
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header class="entry-header">
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( 'Permalink to %s', the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-			<p>by <?php planet3_0_posted_by(); ?></p>
+			<p class="entry-meta"><?php planet3_0_posted_on(); ?> | <?php planet3_0_posted_by(); ?></p>
 		</header><!-- .entry-header -->
 		<div class="entry-summary">
 			<?php the_excerpt(); ?>
